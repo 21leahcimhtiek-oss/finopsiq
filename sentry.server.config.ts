a15001dc2aa1629f76305c6a4dc5a8dfc0ea0c1d
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 17:11:50 -0700

    feat: add sentry.server.config.ts

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,6 +2,11 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0,
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0,
   debug: false,
+  beforeSend(event) {
+    delete event.user;
+    return event;
+  },
 });
